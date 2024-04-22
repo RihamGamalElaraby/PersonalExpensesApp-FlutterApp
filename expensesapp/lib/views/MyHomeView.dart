c57c5da981a1f825761b8b31536bbd3a350cba46
RehamElaraby <rehamelaraby2272@gmail.com>
2024-04-22 02:19:48 +0200

    first update

--- expensesapp/lib/views/MyHomeView.dart
+++ expensesapp/lib/views/MyHomeView.dart
@@ -1,8 +1,22 @@
+import 'package:expensesapp/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 class MyHomeView extends StatelessWidget {
-  const MyHomeView({super.key});
-
+  MyHomeView({super.key});
+  final List<Transaction> transations = [
+    Transaction(
+      id: '1',
+      amount: 60,
+      date: DateTime.now(),
+      title: 'new shirt',
+    ),
+    Transaction(
+      id: '2',
+      amount: 80.00,
+      date: DateTime.now(),
+      title: 'new shirt',
+    )
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,17 +25,68 @@ class MyHomeView extends StatelessWidget {
             child: Text('My Expenses'),
           ),
         ),
-        body: Column(children: [
-          Container(
-            child: Card(
-              color: Colors.blue,
-              elevation: 5,
-              child: Text('CHART!'),
-            ),
-          ),
-          Card(
-            child: Text('List of TX!'),
-          )
-        ]));
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  elevation: 5,
+                  child: Text('CHART!'),
+                ),
+              ),
+              Column(
+                children: transations
+                    .map((tx) => Card(
+                          child: Row(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color: Colors.black,
+                                      width: 2,
+                                      style: BorderStyle.solid),
+                                  // color: Colors.yellow,
+                                ),
+                                margin: EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 15),
+                                child: Text(
+                                  tx.amount.toString(),
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      color: Colors.purple),
+                                ),
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    tx.title,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                        color: const Color.fromARGB(
+                                            255, 35, 35, 35)),
+                                  ),
+                                  Text(
+                                    tx.date.toString(),
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                        color: Colors.grey),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ))
+                    .toList(),
+              )
+            ]));
   }
 }
